850b99a0f7af1a2cfccb99b866860501ecf80dfe
Seanghost117 <36561158+Seanghost117@users.noreply.github.com>
2019-07-28 17:46:34 -0400

    Started example

---
+++ Source/DContent.h
@@ -0,0 +1,9 @@
+//this is our features namespace call it by Features::(Feature name)
+namespace Features {
+	void UpdateLoop();
+  
+  //example
+  extern bool sixstar_bool;
+  void sixstar(bool toggle);
+  
+}
