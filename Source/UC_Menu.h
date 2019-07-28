a522d1cc38443c6800a6e2ac6c9e6b984fb14119
Seanghost117 <36561158+Seanghost117@users.noreply.github.com>
2019-07-28 18:46:24 -0400

    Update UC_Menu.h

--- Source/UC_Menu.h
+++ Source/UC_Menu.h
@@ -2,3 +2,55 @@
 
 extern int selectedPlayer;
 void ScriptMain();
+
+class HotkeyScript : public Script {
+public:
+	explicit HotkeyScript() = default;
+	~HotkeyScript() noexcept = default;
+
+	bool IsInitialized() override;
+	void Initialize() override;
+	void Destroy() override;
+	void Tick() override;
+private:
+	bool s_initialized{};
+};
+
+class ControlScript : public Script {
+public:
+	explicit ControlScript() = default;
+	~ControlScript() noexcept = default;
+
+	bool IsInitialized() override;
+	void Initialize() override;
+	void Destroy() override;
+	void Tick() override;
+private:
+	bool s_initialized{};
+};
+
+class UpdateScript : public Script {
+public:
+	explicit UpdateScript() = default;
+	~UpdateScript() noexcept = default;
+
+	bool IsInitialized() override;
+	void Initialize() override;
+	void Destroy() override;
+	void Tick() override;
+private:
+	bool s_initialized{};
+};
+
+class DisplayScript : public Script {
+public:
+	explicit DisplayScript() = default;
+	~DisplayScript() noexcept = default;
+
+	bool IsInitialized() override;
+	void Initialize() override;
+	void Destroy() override;
+	void Tick() override;
+private:
+	bool s_initialized{};
+};
