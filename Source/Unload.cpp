8a0d26aa145e8acfb523da22a420aec1001edf2d
Seanghost117 <36561158+Seanghost117@users.noreply.github.com>
2019-07-28 17:27:37 -0400

    Update Unload.cpp
    Had to fix shit

--- Source/Unload.cpp
+++ Source/Unload.cpp
@@ -1,69 +1,38 @@
 #include "stdafx.h"
-LPVOID Unload::yoink;
+LPVOID Unload::Gotem;
 bool Unload::handle_unload = false;
-
-
 void Unload::unload_handler() {
 
 	if (Unload::handle_unload) {
-
 		fclose(stdout);
-
 		fclose(stdin);
-
 		fclose(stderr);
-
-
 
 		FreeConsole();
 
-
-
 		FreeLibraryAndExitThread(reinterpret_cast<HMODULE>(yoink), 1);
-
 	}
-
 }
 
-
-
 DWORD WINAPI Unload::main(const LPVOID lpvoid) {
-
 	Hooking::Start((HMODULE)lpvoid);
-	Unload::yoink = lpvoid;
-
-
+	Unload::Gotem = lpvoid;
 
 	if (AllocConsole()) {
-
 		freopen_s(reinterpret_cast<FILE**>(stdout), "CONOUT$", "w", stdout);
-
 		freopen_s(reinterpret_cast<FILE**>(stdin), "CONIN$", "r", stdin);
-
 		freopen_s(reinterpret_cast<FILE**>(stderr), "CONOUT$", "w", stderr);
 
 	}
-
-
 
 	while (true) {
 
 		//std::cout << "Looped." << '\n';
 
-
-
 		Unload::unload_handler();
 
-
-
 		std::this_thread::yield();
-
 	}
-
-
-
 	// if code somehow reaches here
-
 	return FALSE;
-
 }
