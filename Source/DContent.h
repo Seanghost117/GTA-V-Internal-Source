277a33c63a6586a3c2ad7e35c0bdd3b15354bf6f
Seanghost117 <36561158+Seanghost117@users.noreply.github.com>
2019-07-28 18:49:59 -0400

    Update DContent.h

--- Source/DContent.h
+++ Source/DContent.h
@@ -1,9 +1,10 @@
 //this is our features namespace call it by Features::(Feature name)
 namespace Features {
 	void UpdateLoop();
-  
+  	int main();
+	extern bool userHotkey;
   //example
-  extern bool sixstars_bool;
-  void sixstars(bool toggle);
+ 	extern bool sixstars_bool;
+	void sixstars(bool toggle);
   
 }
