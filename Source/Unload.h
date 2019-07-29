d0850abb16b9751d442201f6430b6fc4f8b1e554
Pocakking <42542447+Pocakking@users.noreply.github.com>
2019-07-29 13:07:53 +0200

    Make the menu not suck

--- Source/Unload.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-//K Don't need any useless mismatching
-namespace Unload {
-	extern LPVOID Gotem;
-	extern bool handle_unload;
-	void unload_handler();
-	DWORD WINAPI main(const LPVOID lpvoid);
-}
